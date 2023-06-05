c6138d4c01177fcd10bb0da4f086aaa1563709bb
Aleksey Smirnov <lexa.smirnov.00@mail.ru>
2023-06-05 17:54:12 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ru.chat.chat-backend"
---
+++ src/main/kotlin/ru/chat/Connection.kt
@@ -0,0 +1,11 @@
+package ru.chat
+
+import io.ktor.websocket.*
+import java.util.concurrent.atomic.*
+
+class Connection(val name: String, val session: DefaultWebSocketSession) {
+    companion object {
+        val lastId = AtomicInteger(0)
+    }
+    val id = lastId.get()
+}
